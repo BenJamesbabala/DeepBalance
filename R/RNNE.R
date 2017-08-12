c0b2b72d29d2d794e27e3904db8ec961f03b70a9
Peter Xenopoulos <peter.xeno@gmail.com>
2017-08-12 16:25:28 -0400

    RNNE() lintr pass

--- R/RNNE.R
+++ R/RNNE.R
@@ -1,5 +1,6 @@
 #' @title Ensemble of Random Neural Networks
-#' @description This function trains an ensemble of random neural networks using balanced bootstrapped data
+#' @description This function trains an ensemble of random neural networks 
+#'     using balanced bootstrapped data
 #' @author Peter Xenopoulos 
 #' 
 #' @param formula Formula for containing outcome and all of the predictors of interest
@@ -16,22 +17,29 @@
 #' @examples
 #' RNNE(formula, train, mtry = 3, total.nets = 10, verbose = TRUE, n.cores = 1)
 
-RNNE <- function(formula, train, mtry = 1, total.nets = 1, verbose = FALSE, n.cores = 1) {
-  
-  # Trains an ensemble of random neural networks using balanced bootstrapped data
+RNNE <- function(formula,
+                 train,
+                 mtry = 1,
+                 total.nets = 1,
+                 verbose = FALSE,
+                 n.cores = 1) {
+
+  # Trains an ensemble of random neural networks
+  #    using balanced bootstrapped data
   #
   # Args:
   #   formula    : Formula used for model, e.g. y~x1+x2+x3
   #   train      : Training data set
-  #   mtry       : Number of randomly selected variables to try for each individual neural network
+  #   mtry       : Number of randomly selected variables to try for
+  #                   each individual neural network
   #   total.nets : Total number of neural networks to use
   #   verbose    : Whether or not to be verbose with output
   #   n.cores    : Number of cores to use (if parallel)
   #
   # Returns:
   #   Returns list of trained neural networks
-  
+
   # Error Handling
-  
+
   return(TRUE)
 }
